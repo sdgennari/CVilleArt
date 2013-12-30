35994ffdac1665124051a3c9f626eb3742eea855
Alex Ramey <alex.b.ramey@gmail.com>
2013-12-30 00:33:04 -0500

    Setup Framework for Transportation Fragment's Display with Scrollable Tabs

--- src/com/hooapps/pca/cvilleart/TransportationFragment.java
+++ src/com/hooapps/pca/cvilleart/TransportationFragment.java
@@ -12,19 +12,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+
 import com.hooapps.pca.cvilleart.R;
 
 // TODO Update the JavaDoc description as the functionality increases
 
 /**
  * Fragment to provide information about various transportation routes to and
- * from art venues. [MORE HERE]
+ * from art venues. This Fragment has a TabHost which contains a TabWidget wrapped
+ * in a HorizontalScrollView that holds tabs and a FrameLayout in which tab content is displayed.
+ * Tabs are added by invoking TabHost.addTab(TabSpec), where the TabSpec defines
+ * the tab's tag that is passed to the TabContentFactory's public View createTabContent(String tag)
+ * method, the tab's content (an instance of a TabContentFactory), and the tab's 
+ * indicator (String Label).
  * 
  * @author Spencer Gennari
+ * @author Alex Ramey
  *
  */
 
 public class TransportationFragment extends Fragment {
+	private TabHost tabHost;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		// If the activity is being recreated, restore previous version
@@ -51,6 +61,31 @@ public class TransportationFragment extends Fragment {
 		} else {
 			// TODO Setup the fragment according to other specifications
 		}
+		
+		tabHost = (TabHost) this.getView().findViewById(R.id.tabhost);
+		TransportationViewFinder finder = new TransportationViewFinder(LayoutInflater.from(this.getActivity()));
+		
+		TabSpec publicTrans = tabHost.newTabSpec("public");
+		publicTrans.setIndicator("Public Transportation");
+		publicTrans.setContent(finder);
+		
+		TabSpec parking = tabHost.newTabSpec("parking");
+		parking.setIndicator("Parking");
+		parking.setContent(finder);
+		
+		TabSpec taxi = tabHost.newTabSpec("taxi");
+		taxi.setIndicator("Taxi");
+		taxi.setContent(finder);
+		
+		TabSpec bike = tabHost.newTabSpec("bike");
+		bike.setIndicator("Bike Rental");
+		bike.setContent(finder);
+		
+		tabHost.setup();
+		tabHost.addTab(publicTrans);
+		tabHost.addTab(parking);
+		tabHost.addTab(taxi);
+		tabHost.addTab(bike);
 	}
 	
 	@Override
